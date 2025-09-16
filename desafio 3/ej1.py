7c4c7224dd688cacd93dfbdedfb4e3a880eea1c4
elamasprojects <ezequiellamas@gmail.com>
2025-09-16 13:40:59 -0300

    Add logic to assign food items for party guests and display menu options

--- desafio 3/ej1.py
+++ desafio 3/ej1.py
@@ -6,6 +6,18 @@ print(f"Vendrán {amigos} amigos a la fiesta")
 
 i = 0
 
+# LOGICA DEL WHILE: CREAMOS POR CADA PERSONA UN NUMERO RANDOM, Y CADA NUMERO CORRESPONDE A UNA COMIDA PARTICULAR.
+# DE ESA FORMA ASIGNAMOS COMIDAS Y LAS MOSTRAMOS
+
+# inicializamos variabels para saber si se traen estos ingredientes o no
+
+prepizza = False
+salsa = False
+muzzarella = False
+coca = False
+ron = False
+whisky = False
+
 while i < amigos:
 
     opcion = random.randint(1,6) #generamos una opcion aleatoria
@@ -16,28 +28,88 @@ while i < amigos:
 
         comida = "prepizza"
 
+        prepizza = True
+
     elif opcion == 2:
 
         comida = "salsa de tomate"
+
+        salsa = True
+        
         
     elif opcion == 3:
 
         comida = "muzzarella"
 
+        muzzarella = True
+
     elif opcion == 4:
 
         comida = "coca"
+
+        coca = True
+
 
     elif opcion == 5:
 
         comida = "ron"
 
+        ron = True
+
+
     else:
 
         comida = "whisky"
 
+        whisky = True
 
-    print(comida)
+
+
+    print(f"Amigo trae {comida}")
 
     i += 1
 
+pizza = False
+roncola = False
+whiscola = False
+
+print ("Menu")
+print ("====")
+
+
+print("Bebidas:")
+
+if coca:
+
+    print ("Coca-Cola")
+
+if whisky:
+
+    print ("Whisky")
+
+if ron:
+
+    print ("Ron")
+
+if coca and ron:
+
+    roncola = True
+    
+    print ("Roncola")
+
+
+if whisky and coca:
+
+    whiscola = True
+
+    print("Whiscola")
+
+if prepizza and salsa and muzzarella:
+
+    pizza = True
+
+    print("Comida:")
+
+    print("Pizza")
+else:
+    print("No hay comida")
---
+++ desafio 3/ej2.py
@@ -0,0 +1,29 @@
+"""
+1. Solicitar dos numeros enteros positivos. 
+1.2 Validar:
+
+    1) El segundo debe ser mayor o igual que el segundo
+    2) Positivos
+    3) Numeros
+
+2. Mostrar mensaje de error si no se cumplen y reingresar
+3. Defininr intervalo usando ambos numeros como extremos
+4. Buscar numero primo mas grande del intervalo
+5. Buscar numero primo mas chico del intervalo
+6. Si no hay ninguno, mostrarlo
+
+
+"""
+
+inicio = int(input("Ingresa el primer numero"))
+
+while inicio < 0 or 
+
+fin = input("Ingrsa el segundo número")
+
+while fin < inicio or fin < 0:
+
+    print("Error, ingresa otro numero")
+
+    fin = input("Ingrsa el segundo número")
+

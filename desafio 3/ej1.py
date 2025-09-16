da022a839e88085e1338cdd2a7e155bcefd4c460
elamasprojects <ezequiellamas@gmail.com>
2025-09-16 13:13:33 -0300

    Implement food selection logic for party guests in Python script

--- desafio 3/ej1.py
+++ desafio 3/ej1.py
@@ -4,3 +4,40 @@ amigos = random.randint(4,10) #definimos cantidad de amigos
 
 print(f"Vendrán {amigos} amigos a la fiesta")
 
+i = 0
+
+while i < amigos:
+
+    opcion = random.randint(1,6) #generamos una opcion aleatoria
+
+    comida = ""
+
+    if opcion == 1:
+
+        comida = "prepizza"
+
+    elif opcion == 2:
+
+        comida = "salsa de tomate"
+        
+    elif opcion == 3:
+
+        comida = "muzzarella"
+
+    elif opcion == 4:
+
+        comida = "coca"
+
+    elif opcion == 5:
+
+        comida = "ron"
+
+    else:
+
+        comida = "whisky"
+
+
+    print(comida)
+
+    i += 1
+
